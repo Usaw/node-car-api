37d1ecd2235fb43d28a702163ff5c2bf34634da4
Usaw <wilhem.guillaume@bbox.fr>
2018-03-18 19:12:24 +0100

    feat(es): recovery of brands

--- src/api.js
+++ src/api.js
@@ -1,11 +1,18 @@
 const {CAR_DYNAMIC_API} = require('./constants');
 const post = require('./post');
-
+const {getBrands} = require('node-car-api');
+const {getModels} = require('node-car-api');
+const fs = require('fs');
+//var express = require('express');
+//var request = require('request');
+//var cheerio = require('cheerio');
+//var app     = express();
 /**
  * Parse response to get brands
  * @param  {String} body
  * @return {Array}
  */
+
 const parse = response => {
   try {
     const text = JSON.parse(response.text);
@@ -30,4 +37,34 @@ module.exports = async (payload, configuration = {}) => {
   } catch (e) {
     return Promise.reject(e);
   }
+
 };
+
+async function printBrands () {
+  const brands = await getBrands();
+  fs.writeFile('brands.json', JSON.stringify(brands, null, 4), function(err)
+                        {
+                            console.log('Success.');
+                        });
+
+console.log("brands: ");
+  console.log(brands);
+
+}
+
+printBrands();
+
+
+
+/*async function printModels () {
+  const models = await getModels('PEUGEOT');
+  console.log("models: ");
+    console.log(models);
+
+}
+
+printModels();*/
+/*fs.writeFile('brands.json', JSON.stringify(json, null, 4), function(err)
+                      {
+                          console.log('Success.');
+                      });*/
